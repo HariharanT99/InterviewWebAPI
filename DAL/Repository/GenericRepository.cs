64a48c805066b0a03e46840e710c7989abf27b7f
Mohammed Thahseen Niaz Ahamed <thahseenn@gislen.com>
2021-11-11 11:17:50 +0530

    added dbconnection variable in generic repo

--- DAL/Repository/GenericRepository.cs
+++ DAL/Repository/GenericRepository.cs
@@ -1,9 +1,17 @@
+using System.Data;
 using IDAL.IRepository;
 
 namespace DAL.Repository
 {
     public class GenericRepository : IGenericRepository
     {
+
+        protected IDbConnection Connection { get; private set; }
+
+        public GenericRepository(IDbConnection connection)
+        {
+            this.Connection = connection;
+        }
 
     }
 }
